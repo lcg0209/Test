39afe81005e9a2489f83c6bbebcce96b52bbaa8f
lcg0209 <cg.lee@dandinet.com>
2014-09-23 12:05:51 +0900

    second commit

---
+++ src/com/example/test/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		LinearLayout linear = (LinearLayout)findViewById(R.id.linear);
+		linear.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(event.getAction()==MotionEvent.ACTION_DOWN){
+					TextView text = (TextView)findViewById(R.id.text);
+					text.setText("Touched");
+					return true;
+				}
+				return false;
+			}
+		});
+		
+	}
+
+}
